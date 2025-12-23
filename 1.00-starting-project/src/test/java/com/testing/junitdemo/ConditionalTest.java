2e3a24cc6906979ab1120b6854516ef6b082c9a2
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-23 23:23:20 +0100

    Test For Windows

--- 1.00-starting-project/src/test/java/com/testing/junitdemo/ConditionalTest.java
+++ 1.00-starting-project/src/test/java/com/testing/junitdemo/ConditionalTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ConditionalTest {
@@ -12,5 +14,11 @@ public class ConditionalTest {
     @Disabled("Don't run this test")
     void basicTest() {
         //Execute something
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testForWindows() {
+        System.out.println("Enabled on Windows");
     }
 }
