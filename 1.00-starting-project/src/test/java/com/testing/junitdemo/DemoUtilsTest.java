60bcdcefa350495492f214423ded40735c35ab8b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-23 17:06:32 +0100

    Setup

---
+++ 1.00-starting-project/src/test/java/com/testing/junitdemo/DemoUtilsTest.java
@@ -0,0 +1,50 @@
+package com.testing.junitdemo;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class DemoUtilsTest {
+
+    private DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
+
+    @Test
+//    @DisplayName("Equals and Not Equals")
+    void test_equals_and_not_equals() {
+
+        assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+    }
+
+    @Test
+//    @DisplayName("Null and Not Null")
+    void test_null_and_not_null() {
+
+        String str1 = null;
+        String str2 = "cosik";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+    }
+
+    /*@AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach\n");
+    }
+
+    @BeforeAll
+    static void setupBeforeAll() {
+        System.out.println("@BeforeAll executes only once before all test methods");
+    }
+
+    @AfterAll
+    static void setupAfterAll() {
+        System.out.println("@AfterAll executes only once after all test methods");
+    }*/
+}
