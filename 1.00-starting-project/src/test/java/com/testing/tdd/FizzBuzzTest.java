9bdebcc73b018781fd79f815063c9dacb86b033f
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-24 00:37:43 +0100

    Tests for FizzBuzz

--- 1.00-starting-project/src/test/java/com/testing/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/testing/tdd/FizzBuzzTest.java
@@ -1,4 +1,49 @@
 package com.testing.tdd;
 
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FizzBuzzTest {
+
+    @Test
+    @Order(1)
+    @DisplayName("Divisible by Three")
+    void testForDivisibleByThree() {
+
+        String expected = "Fizz";
+
+        assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Divisible by Five")
+    void testForDivisibleByFive() {
+
+        String expected = "Buzz";
+
+        assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Divisible by Three And Five")
+    void testForDivisibleByThreeAndFive() {
+
+        String expected = "FizzBuzz";
+
+        assertEquals(expected, FizzBuzz.compute(15), "Should return FizzBuzz");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Not Divisible")
+    void testNotDivisible() {
+
+        String expected = "13";
+
+        assertEquals(expected, FizzBuzz.compute(13), "Should return 13");
+    }
 }
