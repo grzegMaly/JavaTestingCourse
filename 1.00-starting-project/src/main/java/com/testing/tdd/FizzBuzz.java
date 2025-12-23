fa0f7b0a17319c347bef5b14a5c71f95d0304c8a
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-24 00:37:30 +0100

    FizzBuzz Class with tested Logic

---
+++ 1.00-starting-project/src/main/java/com/testing/tdd/FizzBuzz.java
@@ -0,0 +1,38 @@
+package com.testing.tdd;
+
+public class FizzBuzz {
+
+    /*public static String compute(int i) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(i);
+    }*/
+
+    public static String compute(int i) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.isEmpty()) {
+            result.append(i);
+        }
+        return result.toString();
+    }
+}
