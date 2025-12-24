52b5a5eb1dbd3004cea7326f27b9d7ede11d840e
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-24 16:08:36 +0100

    First Mock Test

--- 2.20-starting-project/src/main/java/com/testing/component/service/ApplicationService.java
+++ 2.20-starting-project/src/main/java/com/testing/component/service/ApplicationService.java
@@ -1,14 +1,18 @@
 package com.testing.component.service;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.testing.component.dao.ApplicationDao;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class ApplicationService {
 
-    @Autowired
     private ApplicationDao applicationDao;
 
     public double addGradeResultsForSingleClass(List<Double> numbers) {
--- 2.20-starting-project/src/test/java/com/testing/test/MockAnnotationTest.java
+++ 2.20-starting-project/src/test/java/com/testing/test/MockAnnotationTest.java
@@ -1,11 +1,22 @@
 package com.testing.test;
 
 import com.testing.component.MvcTestingExampleApplication;
+import com.testing.component.dao.ApplicationDao;
 import com.testing.component.models.CollegeStudent;
 import com.testing.component.models.StudentGrades;
+import com.testing.component.service.ApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class MockAnnotationTest {
@@ -14,8 +25,32 @@ public class MockAnnotationTest {
     ApplicationContext context;
 
     @Autowired
-    CollegeStudent collegeStudent;
+    CollegeStudent studentOne;
 
     @Autowired
     StudentGrades studentGrades;
+
+    @Mock
+    private ApplicationDao applicationDao;
+
+    @InjectMocks
+    private ApplicationService applicationService;
+
+    @BeforeEach
+    void beforeEach() {
+        studentOne.setFirstname("Bob");
+        studentOne.setLastname("Dark");
+        studentOne.setEmailAddress("bob.dark@testing.com");
+        studentOne.setStudentGrades(studentGrades);
+    }
+
+    @Test
+    @DisplayName("When & Verify")
+    void assertEqualsAddGradesTest() {
+        when(applicationDao.addGradeResultsForSingleClass(studentGrades.getMathGradeResults())).thenReturn(100.00);
+        assertEquals(100, applicationService.addGradeResultsForSingleClass(studentGrades.getMathGradeResults()));
+
+        verify(applicationDao).addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+        verify(applicationDao, times(1)).addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+    }
 }
